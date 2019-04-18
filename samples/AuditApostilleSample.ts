8e3974dfe078a2ef9a964f38c240ce5d4571a068
daoka <daoka.sweep@gmail.com>
2019-04-18 11:59:20 +0900

    added get metadata when audit

--- samples/AuditApostilleSample.ts
+++ samples/AuditApostilleSample.ts
@@ -1,9 +1,10 @@
 import { AuditService } from '../src/service/AuditService';
 import { NetworkType } from 'nem2-sdk';
 
-const url = 'http://13.114.200.132:3000';
+const url = 'http://18.217.110.63:3000';
 const auditData = 'aaaaaaaaa';
-const txHash = 'D4F928C2A2F9769D45FC60E748E3F753EA91CB01EA6216A89631C7540743D468';
+const txHash = '5C1FD481BAEE45355E6B182C35F2509EE0A973FD7073CA3ACE7F0F54C8BC5460';
+// const txHash = 'A4F5BF70F97ED7A6CF1A4AD86940394F2468D1B7388DAEE276E3215500A39BAD';
 
 run();
 
@@ -16,4 +17,5 @@ async function run() {
   console.log(`ownerPublicAccount: ${result.ownerPublicAccount!.publicKey}`);
   console.log(`ApostilleAddress: ${result.apostilleAddress!.plain()}`)
   console.log(`timestamp: ${new Date(result.timestamp!)}`);
+  console.log(`metadata: ${result.metadata}`);
 }
