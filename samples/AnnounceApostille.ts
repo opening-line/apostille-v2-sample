f9076888f8b7c3064b2d0e87f77617308ef7c817
daoka <daoka.sweep@gmail.com>
2019-04-22 16:06:35 +0900

    fix typo

--- samples/AnnounceApostille.ts
+++ samples/AnnounceApostille.ts
@@ -9,7 +9,7 @@ const fileData = file.toString('hex');
 const filename = '90681.png';
 const ownerPrivateKey = 'aaaaaaaaaaeeeeeeeeeebbbbbbbbbb5555555555dddddddddd1111111111aaee';
 const sha256 = new SHA256();
-const url = 'http://13.114.200.132:3000';
+const url = 'https://jp5.nemesis.land:3001';
 const metadata = { filename: '90681.jpeg', description: "daoka icon" };
 
 const apostilleService = new ApostilleService(fileData, filename,
--- samples/AuditApostilleSample.ts
+++ samples/AuditApostilleSample.ts
@@ -2,7 +2,7 @@ import { AuditService } from '../src/service/AuditService';
 import { NetworkType } from 'nem2-sdk';
 import * as fs from 'fs';
 
-const url = 'http://13.114.200.132:3000';
+const url = 'https://jp5.nemesis.land:3001';
 const file = fs.readFileSync(`${__dirname}/file/90681.jpeg`);
 const fileData = file.toString('hex');
 const txHash = '2A48C0E6E5AA568A4AAEBCFFFE1584E11BC3E427549BA63FC7EE0849A30C51B4';
@@ -16,7 +16,7 @@ async function run() {
   console.log(`txHash: ${result.txHash}`);
   console.log(`fileHash: ${result.fileHash}`);
   console.log(`ownerPublicAccount: ${result.ownerPublicAccount!.publicKey}`);
-  console.log(`ApostilleAddress: ${result.apostilleAddress!.plain()}`)
+  console.log(`ApostilleAddress: ${result.apostilleAddress!.plain()}`);
   console.log(`timestamp: ${new Date(result.timestamp!)}`);
   console.log(`metadata: ${result.metadata}`);
 }
