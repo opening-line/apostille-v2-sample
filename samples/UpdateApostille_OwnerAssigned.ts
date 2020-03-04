a35f934728721c767aba2e9ad6a003fb2a2836bc
daoka <daoka.sweep@gmail.com>
2020-03-04 16:22:40 +0900

    refactor: 💡 added update apostille for owner assigned sample

--- samples/UpdateApostille_OwnerAssigned.ts
+++ samples/UpdateApostille_OwnerAssigned.ts
@@ -1,26 +1,26 @@
-import { CreateApostilleService }  from '../src/service/CreateApostilleService';
+import { UpdateApostilleService }  from '../src/service/service';
 import { SHA256 } from '../src/hash/hash';
-import { NetworkType } from 'nem2-sdk';
+import { NetworkType, PublicAccount } from 'nem2-sdk';
 import * as fs from 'fs';
 
 console.log(__dirname);
 const file = fs.readFileSync(`${__dirname}/file/90681.jpeg`);
 const fileData = file.toString('hex');
-const filename = `${Math.random().toString(32).substring(2)}.png`;
 const ownerPrivateKey = 'DFD40EE55EB215FF96ABCFC8D3E4E02EF77B4158306725DBF4243C91D8CBA774';
 const sha256 = new SHA256();
 const url = 'https://sym-test.opening-line.jp:3001';
 const networkGenerationHash = '45870419226A7E51D61D94AD728231EDC6C9B3086EF9255A8421A4F26870456A';
-const metadata = { filename: '90681.jpeg', description: 'daoka icon' };
+const apostilleAccountPubKey = '0F91C2379DA33DA28EDEEED890E1AB557FEEFDFFA44E57A76FA98AC72CD9F4C6';
+const apostilleAccount = PublicAccount.createFromPublicKey(apostilleAccountPubKey,
+                                                           NetworkType.TEST_NET);
 
-const apostilleService = CreateApostilleService.create(fileData, filename,
+const apostilleService = UpdateApostilleService.create(fileData,
                                                        sha256, ownerPrivateKey,
+                                                       apostilleAccount,
                                                        url, NetworkType.TEST_NET,
-                                                       networkGenerationHash, 1000);
+                                                       networkGenerationHash, 150);
 
 apostilleService.addAnnouncePublicSinkTransaction();
-apostilleService.addAssignOwnershipTransaction();
-apostilleService.addMetadataTransactions(metadata);
 apostilleService.announce().then(
   (x) => {
     console.log(`txhash: ${x.txHash}`);
