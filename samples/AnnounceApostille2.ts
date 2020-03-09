c6478e6230fbc15b2d77646846e0208502963581
daoka <daoka.sweep@gmail.com>
2020-03-09 20:25:20 +0900

    fix: 🐛 modify sink address to new key derivation
    BREAKING CHANGE: 🧨 Can use only 0.9.3.1
    
    ✅ Closes: #43

--- samples/AnnounceApostille2.ts
+++ samples/AnnounceApostille2.ts
@@ -12,16 +12,16 @@ const ownerPrivateKey = '43E472BE7DCDD9A027F1088CB332E7F755671BE9A72571712F305B9
 const sha256 = new SHA256();
 const url = 'https://sym-test02.opening-line.jp:3001';
 const networkGenerationHash = '44D2225B8932C9A96DCB13508CBCDFFA9A9663BFBA2354FEEC8FCFCB7E19846C';
-// const metadata = { filename: '90681.jpeg', description: 'daoka icon' };
+const metadata = { filename: '90681.jpeg', description: 'daoka icon' };
 
 const apostilleService = CreateApostilleService.create(fileData, filename,
                                                        sha256, ownerPrivateKey,
                                                        url, NetworkType.TEST_NET,
                                                        networkGenerationHash, 1000);
 
-// apostilleService.addAnnouncePublicSinkTransaction();
-// apostilleService.addAssignOwnershipTransaction();
-// apostilleService.addMetadataTransactions(metadata);
+apostilleService.addAnnouncePublicSinkTransaction();
+apostilleService.addAssignOwnershipTransaction();
+apostilleService.addMetadataTransactions(metadata);
 
 const listener = new Listener(url);
 listener.open().then(() => {
